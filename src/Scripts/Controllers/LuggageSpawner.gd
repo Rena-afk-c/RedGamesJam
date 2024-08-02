5b8af9efc038a16a2c2ce45b158f6a6356c87077
Miisan <128460054+Rena-afk-c@users.noreply.github.com>
2024-08-03 00:05:54 +0800

    Feat: Working iSh Conveyer Belt

--- src/Scripts/Controllers/LuggageSpawner.gd
+++ src/Scripts/Controllers/LuggageSpawner.gd
@@ -1,16 +1,48 @@
-# LuggageSpawner.gd
 extends Node2D
 
-@export var spawn_interval: float = 3.0  # Seconds
-@export var luggage_scene: PackedScene
+var luggage_scene = preload("res://src/Nodes/World/Luggage.tscn")
+@export var path: Path2D  
+@onready var spawn_point = $SpawnPoint  
 
-@onready var conveyor_belt: Path2D = get_parent().get_node("ConveyorBelt")
+var spawn_timer = 0
+var spawn_interval = 2.0  
+var move_speed = 50  
+var min_distance = 100  
 
-func _ready() -> void:
-	$Timer.wait_time = spawn_interval
-	$Timer.start()
+var luggage_list = []
 
-func _on_Timer_timeout() -> void:
-	var luggage = luggage_scene.instantiate()
-	luggage.progress = 0  # Start at the beginning of the path
-	conveyor_belt.path_follow.add_child(luggage)
+func _process(delta):
+	spawn_timer += delta
+	if spawn_timer >= spawn_interval and can_spawn_luggage():
+		spawn_luggage()
+		spawn_timer = 0
+	
+	move_luggage(delta)
+
+func can_spawn_luggage() -> bool:
+	if luggage_list.is_empty():
+		return true
+	var last_luggage = luggage_list[luggage_list.size() - 1]
+	return last_luggage.progress > min_distance
+
+func spawn_luggage():
+	var new_luggage = luggage_scene.instantiate()
+	var new_path_follow = PathFollow2D.new()
+	
+	path.add_child(new_path_follow)  
+	new_path_follow.add_child(new_luggage)
+	
+	new_path_follow.progress = 0  
+	new_path_follow.rotation = 0 
+	new_luggage.position = Vector2.ZERO
+	
+	luggage_list.append(new_path_follow)
+
+func move_luggage(delta):
+	for path_follow in luggage_list:
+		path_follow.progress += move_speed * delta
+		path_follow.rotation = 0  
+		
+		if path_follow.progress_ratio >= 1:
+			luggage_list.erase(path_follow)
+			path_follow.queue_free()
