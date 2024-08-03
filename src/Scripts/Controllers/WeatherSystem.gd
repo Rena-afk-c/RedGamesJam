1168efbb9ffb90df9500f6fd345fde4a6471081a
Miisan <128460054+Rena-afk-c@users.noreply.github.com>
2024-08-03 08:49:45 +0800

    Feat: 30% Powerup system

--- src/Scripts/Controllers/WeatherSystem.gd
+++ src/Scripts/Controllers/WeatherSystem.gd
@@ -1,6 +1,7 @@
 extends CanvasModulate
 
 enum WeatherState { DAY, MID, NIGHT }
+@export var enable:bool 
 
 @export var day_color: Color = Color(1.0, 1.0, 1.0, 1.0)
 @export var mid_color: Color = Color(0.8, 0.8, 0.9, 1.0)
@@ -16,22 +17,23 @@ func _ready():
 	color = day_color  
 
 func _process(delta):
-	cycle_timer += delta
-	if cycle_timer >= cycle_duration:
-		cycle_timer = 0.0
-		current_state = WeatherState.DAY
-	
-	var target_color: Color
-	var progress: float = cycle_timer / cycle_duration
-	
-	if progress < 1.0 / 3.0:
-		target_color = day_color.lerp(mid_color, smoothstep(0.0, 1.0 / 3.0, progress) * 3.0)
-	elif progress < 2.0 / 3.0:
-		target_color = mid_color.lerp(night_color, smoothstep(1.0 / 3.0, 2.0 / 3.0, progress) * 3.0 - 1.0)
-	else:
-		target_color = night_color.lerp(day_color, smoothstep(2.0 / 3.0, 1.0, progress) * 3.0 - 2.0)
-	
-	color = color.lerp(target_color, delta / transition_duration)
+	if enable:
+		cycle_timer += delta
+		if cycle_timer >= cycle_duration:
+			cycle_timer = 0.0
+			current_state = WeatherState.DAY
+		
+		var target_color: Color
+		var progress: float = cycle_timer / cycle_duration
+		
+		if progress < 1.0 / 3.0:
+			target_color = day_color.lerp(mid_color, smoothstep(0.0, 1.0 / 3.0, progress) * 3.0)
+		elif progress < 2.0 / 3.0:
+			target_color = mid_color.lerp(night_color, smoothstep(1.0 / 3.0, 2.0 / 3.0, progress) * 3.0 - 1.0)
+		else:
+			target_color = night_color.lerp(day_color, smoothstep(2.0 / 3.0, 1.0, progress) * 3.0 - 2.0)
+		
+		color = color.lerp(target_color, delta / transition_duration)
 
 func smoothstep(edge0: float, edge1: float, x: float) -> float:
 	var t = clamp((x - edge0) / (edge1 - edge0), 0.0, 1.0)
